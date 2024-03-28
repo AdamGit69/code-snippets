e1d0ee1cca34f345353b8f75009439402fb299dd
AdamGit69 <103038993+AdamGit69@users.noreply.github.com>
2024-03-29 10:08:21 +1100

    Create motion_detector_set_state.py

---
+++ motion_detector_set_state.py
@@ -0,0 +1,25 @@
+#==================================================================================================
+#  Save in python_scripts/set_state.py 
+# Set the state or other attributes for the entity specified in the Automation Action
+# more info here: https://github.com/xannor/hass_py_set_state
+#==================================================================================================
+
+inputEntity = data.get('entity_id')
+if inputEntity is None:
+    logger.warning("===== entity_id is required if you want to set something.")
+else:    
+    inputStateObject = hass.states.get(inputEntity)
+    inputState = inputStateObject.state
+    inputAttributesObject = inputStateObject.attributes.copy()
+
+    for item in data:
+        newAttribute = data.get(item)
+        logger.debug("===== item = {0}; value = {1}".format(item,newAttribute))
+        if item == 'entity_id':
+            continue            # already handled
+        elif item == 'state':
+            inputState = newAttribute
+        else:
+            inputAttributesObject[item] = newAttribute
+        
+    hass.states.set(inputEntity, inputState, inputAttributesObject)
